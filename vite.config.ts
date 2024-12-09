643fa6d9eb59df7cba1374d33e09aa4580beae76
Eric Venturino <eric.venturino@orange.fr>
2024-12-09 20:24:10 +0100

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // Dossier de sortie
+    sourcemap: true, // Générer les fichiers de map
+  },
+  server: {
+    port: 3000, // Port pour le dev server
+  },
 });
